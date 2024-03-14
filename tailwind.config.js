4665f6ac9b84212eede39c778dd6aea850901707
wilfred <>
2024-03-14 21:32:43 +0100

    config: add fonts and basic html boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'katibeh': ['katibeh', 'sans-serif'],
+        'montserrat-regular': ['montserrat-regular', 'sans-serif'],
+      },
+      colors: {
+        'elefantes-yellow': '#F3EBCB',
+        'elefantes-orange': '#E5542C',
+        'elefantes-green': '#03576B',
+      }
+    }
   },
   plugins: [],
 }
