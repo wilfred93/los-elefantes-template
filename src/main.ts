c1a7c674875cf789553111f9b31a6eb8df01441d
wilfred <>
2024-03-31 21:23:12 +0200

    feature: add initial config for apartment images slider

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import './output.css'
-let ytShare = document.getElementsByClassName("ytp-share-button")[0]
-console.log(ytShare);
-if (ytShare) {
-    ytShare.remove();
-}
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [Autoplay],
+    slidesPerView: 3,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 0,
+        waitForTransition:true ,
+        reverseDirection: true,
+    },
+    speed:3000,
+    loop: true,
+    spaceBetween: 7,
+
+  });
